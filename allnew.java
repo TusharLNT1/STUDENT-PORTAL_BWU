21a6579bb62592aedfd74516132ca6c16246fe8d
TUSHAR KANTI PATRA <120655185+TusharLNT1@users.noreply.github.com>
2023-05-30 14:09:17 +0530

    Add files via upload

---
+++ allnew.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class allnew
+{
+    public static void MyNewName(int a,float b)
+    {
+        int c=a+b;
+        System.out.println(c);
+        return;
+    }
+     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        MyNewName(c);
+    }
+}
